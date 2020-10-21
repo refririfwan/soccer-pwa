b63a1be2c3ae2bd7b7030d832bd18579f2a0a559
refririfwan <refririfwan@gmail.com>
2020-10-21 14:30:07 +0700

    setup env for dev and prod

---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "soccer-v0.0.2"),
+        filename: "index.js"
+    },
+    module: {
+        rules: [
+            /* style and css loader */
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    /* plugin */
+    plugins: [
+        /* HTML Webpack Plugin */
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html"
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: './src/assets', to: './assets' }
+            ],
+        }),
+    ]
+}
